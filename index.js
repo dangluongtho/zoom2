46f058ce46cb5857e8a3fe513c247f44525349a8
Thomas Dean <dangluongtho@gmail.com>
2017-11-08 17:02:07 +0700

    add view

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
 const searchBook = require('./src/searchopac');
+
+const server1={Url: '140.147.249.38', Port: '7090', Database: 'voyager', Syntax: 'USMARC'};
+const server2={Url: '174.36.57.36', Port: '210', Database: 'oca-americana', Syntax: 'MARC21'};
+
+app.get('/', (req, res) => {
+    const{serverId, title, index} = req.query;
+    const count = 20;
+    const queryText = `@attr 1=4 @attr 4=6 "${title}"`;
+    console.log(`Search on server2 --> ${queryText} FROM INDEX ${index}`)
+    searchBook(server2, queryText, index, count)
+    .then(records => res.render('searchbook', { records }))
+    .catch(err => res.send(err));
+});
 
 app.get('/api/OCA/SearchByTitle', (req, res) => {
     const{serverId, title, index} = req.query;
     const count = 20;
-    const server1={Url: '140.147.249.38', Port: '7090', Database: 'voyager', Syntax: 'USMARC'};
-    const server2={Url: '174.36.57.36', Port: '210', Database: 'oca-americana', Syntax: 'MARC21'};
+    
     const queryText = `@attr 1=4 @attr 4=6 "${title}"`;
     console.log(`Search on server2 --> ${queryText} FROM INDEX ${index}`)
     searchBook(server2, queryText, index, count)
